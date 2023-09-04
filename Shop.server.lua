64c328abb6caa78ce1439b19ab17873813b9b59f
doser225 <68888481+doser225@users.noreply.github.com>
2023-09-04 15:33:20 -0400

    Delete Shop.server.lua

--- Shop.server.lua
+++
@@ -1,7 +0,0 @@
---!! Gamepasses TBD later, create the framework for buying items/upgrades in the shop
-
-
-local ShopFolder = Instance.new('Folder', game.ReplicatedStorage)
-ShopFolder.Name = 'ShopFolder'
-
-
